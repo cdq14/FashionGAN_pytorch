923522db9b172ffe735169141c93a00ad6c91b49
gaohongjie <gaohongjie@bytedance.com>
2018-08-24 22:21:49 +0800

    add config_ih1.py

---
+++ config_ih1.py
@@ -0,0 +1,32 @@
+"""
+usage:
+    from config_ih1 import config
+    print(config.batchSize)
+"""
+
+from easydict import EasyDict
+
+config = EasyDict()
+
+# learning batch
+config.batchSize = 4
+config.test_batchSize = 10
+config.win_size = 128
+
+# specific size
+config.n_map_all = 7
+config.n_condition = 3
+config.n_z = 80
+config.nz = config.n_z
+config.n_c = 3
+config.nc = config.n_c
+config.nt_input = 100
+
+config.lambda_mismatch = 0.1
+config.lambda_fake = 0.9
+
+
+
+
+
+
